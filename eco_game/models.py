69ca38f74995ee5b78a41dd86caa27e26688da65
MultySerey <multyserey@gmail.com>
2024-04-16 12:14:05 +0500

    QnA model

--- eco_game/models.py
+++ eco_game/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    q_id = models.AutoField(primary_key=True)
+    q_text = models.TextField()
+
+    def __str__(self):
+        return self.q_text
+
+
+class Answer(models.Model):
+    a_id = models.AutoField(primary_key=True)
+    q_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    a_text = models.TextField()
+    is_correct = models.BooleanField()
+
+    def __str__(self):
+        return self.a_text
